bf41d26cd681f7c4e6ee3ba5951725a9a2fbe067
Thales Marega <thales.bm92@gmail.com>
2025-09-07 13:14:53 -0300

    added world cup folder

--- src/main.py
+++ src/main.py
@@ -4,15 +4,29 @@ from world_cup.training.linear_regression import LinearRegressionAlgorithm
 from world_cup.training.random_forest import RandomForestAlgorithm
 from world_cup.training.logistic_regression import LogisticRegressionAlgorithm
 
-def main():
+import pandas as pd
+
+def init_world_cup():
     print("Iniciando aplicação")
     
     dataset = load()
-
-    # DecisionTreeAlgorithm(dataset=dataset).training_model()
-    # LinearRegressionAlgorithm(dataset=dataset).training_model()
-    # RandomForestAlgorithm(dataset=dataset).training_model()
+    DecisionTreeAlgorithm(dataset=dataset).training_model()
+    LinearRegressionAlgorithm(dataset=dataset).training_model()
+    RandomForestAlgorithm(dataset=dataset).training_model()
     LogisticRegressionAlgorithm(dataset=dataset).training_model()
 
+def init_covid():
+    print("Iniciando aplicação")
+
+    path = "files/adult_outcome/salary.csv"
+    dataset: pd.DataFrame = pd.read_csv(path)
+
+    print(dataset.head(10))
+
+    print(dataset.columns)
+
+    print(len(dataset))
+
 if __name__ == "__main__":
-    main()
+    # init_world_cup()
+    init_covid()
